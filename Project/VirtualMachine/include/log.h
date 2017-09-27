8372253bcd4b10091bffc42aa4df537ade342aee
Lucas Moretto <lucasmoretto16@gmail.com>
2017-09-27 19:39:44 -0300

    Handling errors as Exceptions

--- Project/VirtualMachine/include/log.h
+++ Project/VirtualMachine/include/log.h
@@ -9,9 +9,9 @@ class Log {
 
     public:
         static void debug(const string &msg);
+        static void error(const string &msg);
         static void info (const string &msg);
         static void warn (const string &msg);
-        static void error(const string &msg);
 };
 
 #endif
--- Project/VirtualMachine/src/log.cpp
+++ Project/VirtualMachine/src/log.cpp
@@ -4,15 +4,15 @@ void Log::debug(const string &msg) {
     fprintf(stdout, "<DEBUG> %s\n", msg.c_str());
 }
 
+void Log::error(const string &msg) {
+    fprintf(stderr, "<ERROR> %s\n", msg.c_str());
+    abort();
+}
+
 void Log::info(const string &msg) {
     fprintf(stdout, "<INFO> %s\n", msg.c_str());
 }
 
 void Log::warn(const string &msg) {
     fprintf(stderr, "<WARN> %s\n", msg.c_str());
-}
-
-void Log::error(const string &msg) {
-    fprintf(stderr, "<ERROR> %s\n", msg.c_str());
-    abort();
 }
