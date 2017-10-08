46cad1fc64f9ae9b20549885222874251d9a9dd6
Lucas Moretto <lucasmoretto16@gmail.com>
2017-10-08 17:51:32 -0300

    Adding comments to Builder

--- Project/Builder/src/builder.py
+++ Project/Builder/src/builder.py
@@ -1,31 +1,59 @@
-# Usage:
-#   (1) $ python builder.py < input.txt > output.cpp
-#   (2) $ python builder.py input.txt > output.cpp
-
 import fileinput
 import sys
 
 
 class Builder:
+    """This class works as a code interpreter.
 
-    TODO = "// Move this file to /VirtualMachine/src so this can work on production"
+    The Builder class can read assembly code files and return its code translation
+    as a source file to be execute by a Virtual Machine.
 
-    INCLUDES = (
+    The input code accepted corresponds to a set of instruction's code and its
+    respectives arguments. The set of instructions will be translated to the
+    Virtual Machine's native language.
+
+    Any blank lines or code comments (lines that starts whit #) will be ignored.
+
+    Attributes:
+        INCLUDES (str): Stores all the dependencies to be linked in the
+                        resulting source file.
+
+        TODO (str):     Represents an observation explaining where the
+                        resulting source file should be placed so it can
+                        works correctly.
+
+    Example:
+        The builder class read a direct input from 'stdin'::
+            $ python builder.py < input.txt > output.cpp
+
+        Or you can simply pass the input file name as an argument to the program::
+            $ python builder.py input.txt > output.cpp
+    """
+
+    __INCLUDES = (
         '#include <vector>\n',
         '#include <code.h>',
         '#include <instruction.h>',
         '#include <machine.h>'
     )
 
+    __TODO = "// Move this file to /VirtualMachine/src so this can work on production"
+
     @classmethod
     def create_header(cls):
-        print(cls.TODO + "\n")
-        for i in cls.INCLUDES:
+        """Creates the resulting source file header, including all its dependencies
+        and observations to explain how it works.
+        """
+        print(cls.__TODO + "\n")
+        for i in cls.__INCLUDES:
             print(i)
         print("\nusing namespace std;\n")
 
     @classmethod
     def create_main(cls):
+        """Creates the main method containing a vector of instructions to be
+        executed by a Virtual Machine
+        """
         print("int main() {\n")
         cls.decode_input()
         print("    Machine m (prog);")
@@ -34,6 +62,12 @@ class Builder:
 
     @classmethod
     def decode_input(cls):
+        """Decodes the assembly code passed as input text to a vector of
+        instructions to be passed to a Virtual Machine.
+
+        Any line in the input that corresponds to a comment or any blank
+        line is ignored in the decode process.
+        """
         ip = 0
         labels = {}
         instructions = []
@@ -65,6 +99,14 @@ class Builder:
 
     @classmethod
     def remove_comments(cls, line):
+        """Remove any comments presents in the specified string.
+
+        Args:
+            line (str): The line of code with comments to be removed.
+
+        Returns:
+            str: The specified line without code comments.
+        """
         if '#' in line:
             line = line[:line.find("#")]
         return line.strip()
@@ -73,4 +115,4 @@ class Builder:
 if __name__ == "__main__":
     Builder.create_header()
     Builder.create_main()
-    sys.exit(0)  # TODO: return something else to indicate exception
+    sys.exit(0)
