cba8710eb879ac82bdc7ac92455ad51a7220ead0
r0zbot <r0zbot@users.noreply.github.com>
2017-10-11 15:01:09 -0300

    Implemented extra atributes

--- Project/VirtualMachine/include/model/entity/robot.h
+++ Project/VirtualMachine/include/model/entity/robot.h
@@ -2,17 +2,24 @@
 #define VIRTUALMACHINE_ROBOT_H
 
 #include <controller/classes/machine.h>
-
 #include <model/interface/entity.h>
 
 class Robot: public Entity {
     private:
         Machine machine;
+        int crystals = 0;
+        int hp = 100;
+        int hpCapacity = 100;
+
+        double fuel = 100;
+        double fuelCapacity = 100;
     public:
         Robot(int id, int posX, int posY, Program prog);
-
+        void clear_crystals();
+        void heal(unsigned int amount);
+        void insert_crystals(unsigned int amount);
+        void refuel(unsigned double amount);
         void replace_machine(Program prog);
-
         void update() override;
 };
 #endif
--- Project/VirtualMachine/src/model/entity/robot.cpp
+++ Project/VirtualMachine/src/model/entity/robot.cpp
@@ -2,8 +2,35 @@
 
 #include <util/config.h>
 
-Robot::Robot(int id, int posX, int posY, Program prog)
+Robot::Robot(const int id,const int posX,const int posY, Program prog)
     : Entity(id, posX, posY), machine(prog, this) {
+
+}
+
+void Robot::clear_crystals() {
+    this->crystals = 0;
+}
+
+void Robot::heal(const unsigned int amount) {
+    if(amount > this->hpCapacity){
+        this->hp = this->hpCapacity;
+    }
+    else{
+        this->hp = amount;
+    }
+}
+
+void Robot::insert_crystals(const unsigned int amount) {
+    this->crystals += amount;
+}
+
+void Robot::refuel(const double amount) {
+    if(amount > this->fuelCapacity){
+        this->fuel = this->fuelCapacity;
+    }
+    else{
+        this->fuel += amount;
+    }
 }
 
 void Robot::replace_machine(const Program prog) {
